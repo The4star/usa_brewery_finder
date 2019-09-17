b0b53d0c21014a3ba5516b96c4d5e302f1103772
root <root@DESKTOP-1ML003O.localdomain>
2019-09-17 19:17:44 +1000

    added functionality and city search

--- index.js
+++ index.js
@@ -9,6 +9,40 @@ const findBrewery = (e) => {
 
     const text = (document.querySelector('[id=search-bar]').value);
     const stateText = (document.querySelector('[id=state-bar]').value);
+    const cityText = (document.querySelector('[id=city-bar]').value);
+
+    if(cityText != "") {
+        info.innerHTML += `<h2 class="centre-text">Showing results for ${cityText}</h2>`
+        const theUrl = `https://api.openbrewerydb.org/breweries?by_city=${cityText}`;
+        const beerUrl = fetch(theUrl);
+        console.log(theUrl)
+        beerUrl
+            .then(response => response.json())
+            .then(data => {
+                if(data.length > 0) {
+                    data.forEach(brewery => {
+                        info.innerHTML += `
+                        <div class="brewery-section">
+                        <h2>${brewery.name}</h2>
+                        <p><strong>website:</strong> <a href="${brewery.website_url}" target="_blank">${brewery.website_url}</a></p>
+                        <p><strong>Type:</strong> ${brewery.brewery_type}</p>
+                        <p><strong>Address:</strong> ${brewery.street}, ${brewery.city}, ${brewery.state}, ${brewery.country} </p>
+                        </div>
+                        `
+                    });
+                } else {
+                    info.innerHTML += "<h2 class='centre-text'>No results found</h2>";
+                };
+            });
+
+    } else {
+        if (text && !stateText) {
+            info.innerHTML += `<h2 class="centre-text">Showing results for ${text}.</h2>`
+        } else if (stateText && !text) {
+            info.innerHTML += `<h2 class="centre-text">Showing results for ${stateText}.</h2>`
+        } else {
+            info.innerHTML += `<h2 class="centre-text">Showing results for ${text}, ${stateText}.</h2>`
+        }
         const theUrl = `https://api.openbrewerydb.org/breweries?by_name=${text}&by_state=${stateText}`;
         const beerUrl = fetch(theUrl);
         console.log(theUrl)
@@ -30,6 +64,8 @@ const findBrewery = (e) => {
                     info.innerHTML += "<h2 class='centre-text'>No results found</h2>";
                 };
             });
+        };
+        searchBar.reset()
 };
 
 
